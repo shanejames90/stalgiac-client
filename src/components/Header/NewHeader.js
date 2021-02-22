1a9c6ea1d0e057d8727ec5e7ecf3d8c367533582
Shane James <shanejames@Shanes-iMac.local>
2021-02-21 22:05:36 -0500

    added sign up modal and new header app bar.

--- src/components/Header/NewHeader.js
+++ src/components/Header/NewHeader.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-// import IconButton from '@material-ui/core/IconButton'
-// import MenuIcon from '@material-ui/icons/Menu'
-// import Nav from 'react-bootstrap/Nav'
 import LoginModal from './LoginModal'
-// import Nav from 'react-bootstrap/Nav'
 import { signIn } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
 import { withSnackbar } from 'notistack'
+import Tooltip from '@material-ui/core/Tooltip'
+import MenuAppBar from './Appbar.js'
+import BottomAppBar from './Bottomnav.js'
 
 const styles = {
   root: {
     flexGrow: 1
+  },
+  bar: {
+    backgroundColor: '#000000'
   },
   grow: {
     flexGrow: 1
@@ -54,6 +56,9 @@ class NewHeader extends React.Component {
     const { history, setUser, enqueueSnackbar } = this.props
     signIn(this.state)
       .then(res => setUser(res.data.user))
+      .then(() => this.setState({ modalOpen: false }))
+      // clear forms
+      .then(() => this.setState({ email: '', password: '' }))
       .then(() => enqueueSnackbar(messages.signInSuccess, {
         variant: 'success'
       }))
@@ -74,38 +79,72 @@ class NewHeader extends React.Component {
   };
 
   render () {
-    const { classes } = this.props
+    const { classes, user } = this.props
+    let unauthenticatedOptions
+    let authenticatedOptions
+    if (!user) {
+      unauthenticatedOptions =
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+                Stalgiac
+          </Typography>
+          <Typography variant="h6" color="inherit" className={classes.menuButton}>
+            <Tooltip title="Home" aria-label="Home">
+              <Button color="inherit" href="#/" style={{ color: '#FFFFFF' }}><span className="material-icons">home</span></Button>
+            </Tooltip>
+          </Typography>
+          <Typography variant="h6" color="inherit" className={classes.menuButton}>
+            <Button color="inherit" onClick={this.handleClickOpen}>
+                Sign In</Button>
+          </Typography>
+          <Typography variant="h6" color="inherit" className={classes.menuButton}>
+            <Button color="inherit" onClick={this.handleSuClickOpen} style={{ color: '#FC4445' }}>
+              Sign Up</Button>
+          </Typography>
+        </Toolbar>
+    }
+    if (user) {
+      authenticatedOptions =
+      <Toolbar>
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+            Stalgiac
+        </Typography>
+        <Typography variant="h6" color="inherit" className={classes.menuButton}>
+          <Tooltip title="Home" aria-label="Home">
+            <Button color="inherit" href="#/" style={{ color: '#FFFFFF' }}><span className="material-icons">home</span></Button>
+          </Tooltip>
+        </Typography>
+        <Typography variant="h6" color="inherit" className={classes.menuButton}>
+          <Tooltip title="Add a Screenshot" aria-label="Add a Screenshot">
+            <Button color="inherit" href="#screenshots" style={{ color: '#16FFBD' }}>
+              <span className="material-icons">add_a_photo</span></Button>
+          </Tooltip>
+        </Typography>
+        <Typography variant="h6" color="inherit" className={classes.menuButton}>
+          <Tooltip title="View your screenshots" aria-label="View your screenshots">
+            <Button color="inherit" href="#index-screenshots" style={{ color: '#FFFFFF' }}>
+              <span className="material-icons">view_list</span></Button>
+          </Tooltip>
+        </Typography>
+        <MenuAppBar/>
+        <BottomAppBar />
+      </Toolbar>
+    }
     return (
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <div className="container">
-              <Link to='/'>
-                <div className="navbar-brand">
-                  <i className="fas fa-globe fa-2x"></i>
-                </div>
-              </Link>
-              <Typography variant="h6" color="inherit" className={classes.grow}>
-                <Button variant="outlined" color="inherit" onClick={this.handleClickOpen}>
-                  Sign In</Button>
-              </Typography>
-              <Typography variant="h6" color="inherit" className={classes.grow}>
-                <a onClick={this.handleModalOpen} className="nav-link">
-                          Sign Up
-                </a>
-              </Typography>
-            </div>
-          </Toolbar>
+      <div className={classes.root}>
+        <AppBar position="static" expand="md" className={classes.bar}>
+          { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+          {user ? authenticatedOptions : unauthenticatedOptions}
+          <LoginModal
+            email={this.state.email}
+            password={this.state.password}
+            open={this.state.modalOpen}
+            handleInputChange={this.handleInputChange}
+            handleClose={this.handleClose}
+            handleClickOpen={this.handleClickOpen}
+            handleSubmit={this.handleSubmit}
+          />
         </AppBar>
-        <LoginModal
-          email={this.state.email}
-          password={this.state.password}
-          open={this.state.modalOpen}
-          handleInputChange={this.handleInputChange}
-          handleClose={this.handleClose}
-          handleClickOpen={this.handleClickOpen}
-          handleSubmit={this.handleSubmit}
-        />
       </div>
     )
   }
